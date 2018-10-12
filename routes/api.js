9493e13a59ae21bdcc565f9f21fc8f12126cc182
Glitch (varner-issue-tracker) <none>
2018-10-12 18:03:33 +0000

    Finished PUT route that updates an issue given its _id

--- routes/api.js
+++ routes/api.js
@@ -75,6 +75,38 @@ module.exports = (app) => {
     
   app.put('/api/issues/:project', (req, res) => {
     let project = req.params.project;
+    let id = req.body._id;
+    let issue_title = req.body.issue_title;
+    let issue_text = req.body.issue_text;
+    let created_by = req.body.created_by;
+    let assigned_to = req.body.assigned_to;
+    let status_text = req.body.status_text;
+    let open = req.body.open;
+    
+    if (issue_title === '' && issue_text === '' && created_by === '' && assigned_to === '' && status_text === '' && open === false) {
+      res.send('No updated field sent.');
+    }
+    
+    let update = {};
+    
+    if (issue_title !== '') update.issue_title = issue_title;
+    if (issue_text !== '') update.issue_text = issue_text;
+    if (created_by !== '') update.created_by = created_by;
+    if (assigned_to !== '') update.assigned_to = assigned_to;
+    if (status_text !== '') update.status_text = status_text;
+    if (open !== false) update.open = false;
+    
+    update.updated_on = Date.now();
+    
+    Issue.findByIdAndUpdate(id, update, (err, updatedIssue) => {
+      if (err) {
+        res.send('Could not update ' + id);
+        return;
+      }
+      
+      res.send('Successfully updated!');
+      return;
+    });
 
   })
     
